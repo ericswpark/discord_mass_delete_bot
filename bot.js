9bbd9590e558d01b2c26ccec7729f6366ff9b8f8
Eric Park <ideamaneric@gmail.com>
2020-01-27 18:11:51 +0800

    Fix nasty logic
    Because of a logic error the bot will actually start deleting everything in a given channel, if there is a message sent. Even if the message does not have the trigger keyword, it will delete everything. Fix this by moving the deletion logic into the correct scope.

--- bot.js
+++ bot.js
@@ -15,28 +15,29 @@ client.on('message', msg => {
         } else {
             isDeleteAlreadyRunning = true;
             msg.channel.send("OK! Deleting all messages in channel...");
+
+            // Channel to delete messages in
+            const targetChannel = msg.channel;
+
+            // Show delete progress
+            targetChannel.startTyping();
+
+            // Get messages from channel
+            targetChannel.fetchMessages({ limit: 100 })
+                .then(messages => {
+                    console.log(`Received ${messages.size} messages`);
+                    messages.forEach(element => {
+                        element.delete(20)
+                            .then(msg => console.log(`Deleted message: ${msg.content}`))
+                            .catch(console.error);
+                    });
+                })
+                .catch(console.error);
+
+            isDeleteAlreadyRunning = false;
+            targetChannel.stopTyping();
         }
     }
-    // Channel to delete messages in
-    const targetChannel = msg.channel;
-
-    // Show delete progress
-    targetChannel.startTyping();
-
-    // Get messages from channel
-    targetChannel.fetchMessages({ limit: 100 })
-        .then(messages => {
-            console.log(`Received ${messages.size} messages`);
-            messages.forEach(element => {
-                element.delete(20)
-                    .then(msg => console.log(`Deleted message: ${msg.content}`))
-                    .catch(console.error);
-            });
-        })
-        .catch(console.error);
-    
-    isDeleteAlreadyRunning = false;
-    targetChannel.stopTyping();
 });
 
 client.login(auth.token);
